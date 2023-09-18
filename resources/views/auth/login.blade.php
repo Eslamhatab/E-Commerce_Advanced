b201d5d5079fad70a3d936d272bd332a81023f48
Kareem-Tarek <kareemtarekpk@gmail.com>
2023-09-18 14:23:19 +0300

    alot of new updates...

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 <div class="container mt-5 mb-5">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
+            <div class="card shadow">
                 <div class="card-header h1 text-center border border-5 shadow rounded-4 font-weight-bold" style="background: linear-gradient(to top left, #466dce, #000000); color: snow;">{{ __('Login') }}</div>
 
                 <div class="card-body">
